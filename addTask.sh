1557c748b4248973420aae5a927d16df710fa155
diegoDAguilar <diego.deaguilarcanellas@telefonica.com>
2017-08-07 11:00:36 +0200

    v3

---
+++ addTask.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+domainName=$1
+lifeTime=$2
+cronTaskID=$3
+day=$(date +%d) #returns current day
+deadLineD=$(date -d "today + $lifeTime days" +'%d') #returns deadline in day month format, e.g. 19 06 as 2 parameters
+deadLineM=$(date -d "today + $lifeTime days" +'%m')
+touch myCron
+crontab -l > myCron
+#Decide best time of the day for the renewal following these:
+echo "50 8 $day * * sh /root/exeAutoRenew.sh  $domainName $deadLineD $deadLineM $cronTaskID" >> myCron
+#echo "0 0 $day * * sh /root/exeAutoRenew.sh  $domainName $deadLineD $deadLineM $cronTaskID" >> myCron 
+crontab myCron
+rm myCron
